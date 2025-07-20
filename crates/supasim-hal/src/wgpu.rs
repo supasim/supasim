6f5806002cd1e500a9d8d13d0d193c35046f2217
SupaMaggie70Incorporated <magnus.larsson.mn@gmail.com>
2025-07-20 13:54:17 -0500

    Improved wgpu buffer writing

--- crates/supasim-hal/src/wgpu.rs
+++ crates/supasim-hal/src/wgpu.rs
@@ -391,12 +391,7 @@ impl BackendInstance<Wgpu> for WgpuInstance {
         offset: u64,
         data: &[u8],
     ) -> Result<(), <Wgpu as Backend>::Error> {
-        let was_mapped = buffer.mapped_slice.is_some();
-        let ptr = unsafe { self.map_buffer(buffer)?.add(offset as usize) };
-        unsafe { std::slice::from_raw_parts_mut(ptr, data.len()) }.clone_from_slice(data);
-        if !was_mapped {
-            unsafe { self.unmap_buffer(buffer)? };
-        }
+        self.queue.write_buffer(&buffer.inner, offset, data);
         Ok(())
     }
 
