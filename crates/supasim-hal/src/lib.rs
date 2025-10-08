dfd5bea35fde2c567789e385fd034ade0ff5b301
Inner Daemons <85136135+inner-daemons@users.noreply.github.com>
2025-10-08 15:38:18 -0500

    New external memory API (#112)
    * Worked on hal API
    
    * Did a little bit of work
    
    * Final API changes for supasim API

--- crates/supasim-hal/src/lib.rs
+++ crates/supasim-hal/src/lib.rs
@@ -184,8 +184,23 @@ pub trait Device<B: Backend<Device = Self>> {
     unsafe fn create_buffer(&self, alloc_info: &HalBufferDescriptor)
     -> Result<B::Buffer, B::Error>;
     /// # Safety
+    /// * Importing buffers is inherently unsafe
+    /// * The driver ID must match
+    /// * The handle must be a valid handle given the backend
+    /// * The offset, size must be valid within the allocation backed by the handle
+    ///  * The memory must be allocated in a reasonable device-side memory location
+    unsafe fn import_buffer(&self, info: &ExternalBufferDescriptor) -> Result<B::Buffer, B::Error>;
+    /// # Safety
     /// Currently no safety requirements. This is subject to change
     unsafe fn create_semaphore(&self) -> Result<B::Semaphore, B::Error>;
+    /// # Safety
+    /// * Importing semaphores is inherently unsafe
+    /// * The driver ID must match (even if semaphores may be shared among devices)
+    /// * The handle must be a valid handle given the backend
+    unsafe fn import_semaphore(
+        &self,
+        info: &ExternalSemaphoreDescriptor,
+    ) -> Result<B::Semaphore, B::Error>;
     /// # Safety
     /// * Must be called after all of the device's queues have been destroyed
     /// * All objects created from this must have been destroyed
@@ -267,7 +282,7 @@ pub enum BufferCommand<'a, B: Backend> {
     /// * This is always safe
     MemoryBarrier { buffer: HalBufferSlice<'a, B> },
     /// # Safety
-    /// * The instance must support the form of import/export used
+    /// * The instance must support importing memory. The buffer must be imported.
     MemoryTransfer {
         buffer: HalBufferSlice<'a, B>,
         import: bool,
